3e6f33aa31042b0dbcc78c7873ed93df5855289b
Josh Guha <joshguha@gmail.com>
2020-02-12 14:55:22 +0000

    Added packages to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./App.css";
+import Header from "./Header";
+import Home from "./Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import Payment from "./Payment";
+import Orders from "./Orders";
+import { auth } from "./firebase";
+import {useStateValue} from "./StateProvider"
+import {loadStripe} from '@stripe/stripe-js'
+import { Elements } from "@stripe/react-stripe-js";
 
 function App() {
 	return (
