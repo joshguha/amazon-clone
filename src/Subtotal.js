4cdd4053dca71da10e22237048364d345e063ad0
Josh Guha <joshguha@gmail.com>
2020-02-14 15:16:40 +0000

    Added CurrencyFormat component into SubTotal

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -11,7 +11,26 @@ function Subtotal() {
 
 	return (
 		<div className="subtotal">
-			<CurrencyFormat />
+			<CurrencyFormat
+				renderText={(value) => (
+					<React.Fragment>
+						<p>
+							Subtotal ({basket.length} items):
+							<strong>{value}</strong>
+						</p>
+						<small className="subtotal__gift">
+							<input type="checkbox" /> This order contains a gift
+						</small>
+					</React.Fragment>
+				)}
+				decimalScale={2}
+				value={getBasketTotal(basket)}
+				displayType={"text"}
+				thousandSeparator={true}
+				prefix={"$"}
+            />
+            
+
 		</div>
 	);
 }
