c4ebcfc61755e7091fbc63885b964e1585e53199
Josh Guha <joshguha@gmail.com>
2020-02-16 15:38:26 +0000

    CheckoutProduct added with ratings

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -11,7 +11,32 @@ function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
 			id: id,
 		});
 	};
-	return <div></div>;
+	return (
+		<div className="checkoutProduct">
+			<img className="checkoutProduct__image" src={image} />
+
+			<div className="checkoutProduct__info">
+				<p className="checkoutProduct__title"> {title} </p>
+				<p className="checkoutProduct__price">
+					<small>$</small>
+					<strong>{price}</strong>
+				</p>
+
+				<div className="checkoutProduct__rating">
+					{Array(rating)
+						.fill()
+						.map((_, i) => (
+							<p>🌟</p>
+						))}
+				</div>
+				{!hideButton && (
+					<button onClick={removeFromBasket}>
+						Remove from Basket
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default CheckoutProduct;
