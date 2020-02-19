b7f063f4d4ce31a01074e5f3e0a1843479b8edc1
Josh Guha <joshguha@gmail.com>
2020-02-19 15:45:37 +0000

    Add to basket dispatch fn added

--- src/Product.js
+++ src/Product.js
@@ -4,4 +4,17 @@ import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
 	const [{ basket }, dispatch] = useStateValue();
+
+	const addToBasket = () => {
+		dispatch({
+			type: "ADD_TO_BASKET",
+			item: {
+				id: id,
+				title: title,
+				image: image,
+				price: price,
+				rating: rating,
+			},
+		});
+	};
 }
