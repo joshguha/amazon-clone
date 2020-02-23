48a065c1ad93fa704738c8ed804848236cda368b
Josh Guha <joshguha@gmail.com>
2020-02-23 16:28:45 +0000

    Payments sections added

--- src/Payment.js
+++ src/Payment.js
@@ -8,6 +8,7 @@ import CurrencyFormat from "react-currency-format";
 import { getBasketTotal } from "./reducer";
 import axios from "axios";
 import { db } from "./firebase";
+import { EventBusy } from "@material-ui/icons";
 
 function Payment() {
 	const [{ basket, user }, dispatch] = useStateValue();
@@ -65,10 +66,52 @@ const handleSubmit = async (event) => {
 
 			dispatch({
 				type: "EMPTY_BASKET",
-            });
-            
+			});
+
 			history.replace("/orders");
 		});
+
+	const handleChange = (event) => {
+		setDisabled(event.empty);
+		setError(event.error ? event.error.message : "");
+	};
+
+	return (
+		<div className="payment">
+			<div className="payment__container">
+				<h1>
+					Checkout (<Link to="/checkout">{basket?.length} items</Link>
+					)
+				</h1>
+
+				<div className="payment__section">
+					<div className="payment__title">
+						<h3>Delivery Address</h3>
+						<p>{user?.email}</p>
+						<p>123 React Lane</p>
+						<p>Los Angeles, CA</p>
+					</div>
+				</div>
+
+				<div className="payment__section">
+					<div className="payment__title">
+						<h3>Review items and delivery</h3>
+					</div>
+					<div className="payment__items">
+						{basket.map((item) => (
+							<CheckoutProduct
+								id={item.id}
+								title={item.title}
+								image={item.image}
+								price={item.price}
+								rating={item.rating}
+							/>
+						))}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Payment;
