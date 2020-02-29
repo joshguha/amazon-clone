6bd11fe2f156fa5f6b1d6aaf2256e6f3cf4059a4
Josh Guha <joshguha@gmail.com>
2020-02-29 16:56:06 +0000

    UseEffect added to Orders component

--- src/Orders.js
+++ src/Orders.js
@@ -5,7 +5,29 @@ import { useStateValue } from "./StateProvider";
 import Order from "./Order";
 
 function Orders() {
+	const [{ basket, user }, dispatch] = useStateValue();
+	const [orders, setOrders] = useState([]);
+
+	useEffect(() => {
+		if (user) {
+			db.collection("users")
+				.doc(user?.uid)
+				.collection("orders")
+				.orderBy("created", "desc")
+				.onSnapshot((snapshot) =>
+					setOrders(
+						snapshot.docs.map((doc) => ({
+							id: doc.id,
+							data: doc.data(),
+						}))
+					)
+				);
+		} else {
+			setOrders([]);
+		}
+    }, [user]);
     
+
 }
 
-export default Orders
+export default Orders;
