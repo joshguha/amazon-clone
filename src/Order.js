ec2b547552b44de631533e7066c742cb9feb9c0c
Josh Guha <joshguha@gmail.com>
2020-02-29 16:59:03 +0000

    Added invidual Order component

---
+++ src/Order.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Order.css";
+import moment from "moment";
+import CheckoutProduct from "./CheckoutProduct";
+import CurrencyFormat from "react-currency-formatÂ";
+
+function Order({ order }) {
+	return <div></div>;
+}
+
+export default Order;
