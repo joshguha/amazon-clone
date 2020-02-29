b66cbdf5164d0777feb2a544da0ab09448886f27
Josh Guha <joshguha@gmail.com>
2020-02-29 16:57:04 +0000

    Orders jsx modified

--- src/Orders.js
+++ src/Orders.js
@@ -29,7 +29,7 @@ function Orders() {
     
 	return (
 		<div className="orders">
-			<h1>Your Order</h1>
+			<h1>Your Orders</h1>
 
 			<div className="orders__order">
 				{orders?.map((order) => (
