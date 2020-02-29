c22c1fe37385b2d890441116a45306ffcb2ef513
Josh Guha <joshguha@gmail.com>
2020-02-29 16:53:19 +0000

    Added Orders component

---
+++ src/Orders.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import { db } from "./firebase";
+import "./Orders.css";
+import { useStateValue } from "./StateProvider";
+import Order from "./Order";
+
+function Orders() {
+    
+}
+
+export default Orders
